92b70f572c61061f35621e5fdc6e595d184b92ec
piyorin <petankosaiko@yahoo.ne.jp>
2020-11-29 17:08:54 +0900

    Needleクラスを追加

---
+++ Assets/Script/Needle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Needle : MonoBehaviour
+{
+    private static string prefabPath = "Prefabs/Needle";
+
+    // 当たり判定
+    private static float range = 50;
+
+    // 座標
+    private Vector2 position;
+
+    /// <summary>
+    /// 指定座標に針を生成
+    /// </summary>
+    /// <param name="position">生成座標</param>
+    /// <returns>生成した針のNeedleクラス</returns>
+    public static Needle instance(Vector2 position)
+    {
+        Debug.Log(string.Format("針が生成された。x:{0} y:{1}", position.x, position.y));
+        GameObject instance = Instantiate((GameObject)Resources.Load(prefabPath));
+        Needle script = (Needle)instance.GetComponent(typeof(Needle).Name);
+        script.position = position;
+        return script;
+    }
+
+    /// <summary>
+    /// 指定座標から一定範囲内にこの針があるか確認
+    /// </summary>
+    /// <param name="position">判定座標</param>
+    /// <returns>一定範囲内にこの針があるならtrue, なければfalse</returns>
+    public bool isCollision(Vector2 position)
+    {
+        return inRange(this.position.x, position.x) && inRange(this.position.y, position.y);
+    }
+
+    // eitherPositionがtargetPosition+-rangeの範囲にあるか判定
+    private bool inRange(float targetPosition, float eitherPosition)
+    {
+        return targetPosition - range <= eitherPosition && eitherPosition <= targetPosition + range;
+    }
+}
--- Assets/Test/NeedleTest.cs
+++ Assets/Test/NeedleTest.cs
@@ -1,28 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System.Reflection;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 namespace Tests
 {
     public class NeedleTest
     {
-        // A Test behaves as an ordinary method
+        // instance
         [Test]
-        public void NeedleTestSimplePasses()
+        public void instance()
         {
-            Debug.Log("test start");
+            const float x = 0;
+            const float y = 0;
+            var instance = Needle.instance(new Vector2(x, y));
+
+            // 生成した針の座標が正しいか確認
+            var targetPosition = getPosition(instance);
+            Assert.AreEqual(targetPosition.x, x);
+            Assert.AreEqual(targetPosition.y, y);
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator NeedleTestWithEnumeratorPasses()
+        // judge 正常系
+        [TestCase(1, 1)]
+        [TestCase(-1, 1)]
+        [TestCase(1, -1)]
+        [TestCase(-1, -1)]
+        public void judge_normal(int xCode, int yCode)
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            // 針を生成し、当たり判定許容範囲を取得する
+            var instance = Needle.instance(new Vector2(0, 0));
+            var range = getRange(instance);
+            // 当たり判定(xとyに1か-1を掛けて上限と下限を調べる)
+            var result = instance.isCollision(new Vector2(range * xCode, range * yCode));
+            Assert.IsTrue(result);
+        }
+
+        // judge 異常系
+        [TestCase(1, 1)]
+        [TestCase(-1, 1)]
+        [TestCase(1, -1)]
+        [TestCase(-1, -1)]
+        public void judge_error(int xCode, int yCode)
+        {
+            // 針を生成し、当たり判定許容範囲を取得する
+            var instance = Needle.instance(new Vector2(0, 0));
+            var range = getRange(instance);
+            // 当たり判定(xとyに1か-1を掛けた上で加算し上限超過と下限未満を調べる)
+            var result = instance.isCollision(new Vector2(range * xCode + xCode, range * yCode + yCode));
+            Assert.IsFalse(result);
+        }
+
+        // instanceのpositionを取得する
+        private Vector2 getPosition(Needle instance)
+        {
+            return (Vector2)instance.GetType().GetField("position", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(instance);
+        }
+
+        // instanceの当たり判定許容範囲を取得する
+        private float getRange(Needle instance)
+        {
+            return (float)instance.GetType().GetField("range", BindingFlags.NonPublic | BindingFlags.Static).GetValue(instance);
         }
     }
 }
